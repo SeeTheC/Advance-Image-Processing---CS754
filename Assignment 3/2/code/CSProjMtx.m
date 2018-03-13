e6ccd690c863f4277aa8b2b0ebd714121d6406cd
Khursheed Ali <khursheed.ali1991@gmail.com>
2018-03-13 23:08:48 +0530

    Done. Only Report is pending

--- Assignment 3/2/code/CSProjMtx.m
+++ Assignment 3/2/code/CSProjMtx.m
@@ -1,4 +1,4 @@
-classdef APartb    
+classdef CSProjMtx    
     properties        
         transpose = 0;
         theta = []; 
@@ -9,14 +9,15 @@ classdef APartb
         w=0;
     end    
     methods      
-       function obj = APartb(m,n,theta)            
+       function obj = CSProjMtx(m,n,theta)            
             obj.m=m;
             obj.n=n;
             obj.h=round(sqrt(n));
             obj.w=round(sqrt(n));
             obj.theta=theta;            
             obj.noOfAngle=numel(theta);
-        end
+       end
+       %  operator * overloading
        function result = mtimes(obj,beta)
             angles=obj.theta;
             if obj.transpose == 0 % A.beta = R.U.Beta= R.x  
@@ -27,12 +28,11 @@ classdef APartb
             else %At.beta
                 % Rt = iradon                
                 projectionMat=reshape(beta,obj.m,obj.noOfAngle);
-                Atx=dct2(iradon(projectionMat,angles,'linear','Ram-Lak',1,obj.h));
-                %Atx=(iradon(projectionMat,angles,'linear','none',1,obj.h));                
-                %iradon(R,0:179,'linear','none',1,180);
+                Atx=dct2(iradon(projectionMat,angles,'linear','Ram-Lak',1,obj.h));               
                 result=reshape(Atx,obj.n,1);
             end
        end  
+       %  operator ' overloading
        function  updatedObj = ctranspose(obj)
            obj.transpose=xor(obj.transpose,1);   
            updatedObj=obj;
